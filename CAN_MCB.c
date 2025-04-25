1e6d6d17b3ae388a2bfa4ea41eff66317cb12a7f
PatricioGarces <patricio.garces@outlook.com>
2025-04-25 00:06:56 -0300

    Soporte para splitBoard

--- CAN_MCB.c
+++ CAN_MCB.c
@@ -3,7 +3,7 @@
 #include "../../../include/main.h"              // TODO: mirar este def
 #include "esp_log.h"
 
-#define PATTERN_QUEUE_SIZE  20
+#define PATTERN_QUEUE_SIZE  100
 
 extern QueueHandle_t newMcbQueueHandler; 
 extern QueueHandle_t motorControlQueueHandler; 
@@ -12,35 +12,85 @@ config_init_mcb_t mcbConfigInit;
 static QueueHandle_t spp_uart_queue;
 static const char *TAG = "CAN_MCB";
 
+uint16_t CalcCRC(uint8_t *ptr, int8_t count) {
+  uint16_t  crc;
+  uint8_t i;
+  crc = 0;
+  while (--count >= 0) {
+    crc = crc ^ (uint16_t) *ptr++ << 8;
+    i = 8;
+    do {
+      if (crc & 0x8000) {
+        crc = crc << 1 ^ 0x1021;
+      }
+      else {
+        crc = crc << 1;
+      }
+    } while(--i);
+  }
+  return (crc);
+}
+
 static void sendMotorData(int16_t motR,int16_t motL,uint8_t enable) {
-    uint16_t calcChecksum = START_CODE_HEADER^motL^motR^enable;
-    tx_motor_control_board_t command = {
-        .start = START_CODE_HEADER,
-        .speedL = motL,
-        .speedR = motR,
-        .enable = enable,
-        .checksum = calcChecksum
-    };
+    #ifdef HARDWARE_SPLITBOARD
+        tx_motor_control_board_t command = {
+            .start = '/',//START_CODE_HEADER,
+            .speedL = motL,
+            .speedR = motR,
+            .unusedStateMaster = 0,
+            .unusedStateSlave = 0,
+            .checksum = CalcCRC((uint8_t*)&command,sizeof(command)-2)
+        };
+    #else
+        tx_motor_control_board_t command = {
+            .start = START_CODE_HEADER,
+            .speedL = motL,
+            .speedR = motR,
+            .enable = enable,
+            .checksum = START_CODE_HEADER^motL^motR^enable
+        };
+    #endif
+
+    // ESP_LOGI(TAG,"MotL: %d \t MotR: %d",motL,motR);
     uart_write_bytes(mcbConfigInit.numUart,&command, sizeof(command));
 }
 
 static void processMCBData(uint8_t *data) {
     rx_motor_control_board_t newMcbData;
-    uint16_t newChecksum = 0;
-
+    
     memcpy(&newMcbData,data,sizeof(rx_motor_control_board_t));
 
     if (newMcbData.start == START_CODE_HEADER) {
+        #ifdef HARDWARE_SPLITBOARD
+            // newChecksum = (uint16_t)(newMcbData.start ^ newMcbData.speedR_meas ^ newMcbData.speedL_meas 
+                                        // ^ newMcbData.batVoltage ^ newMcbData.posL ^ newMcbData.posR ^ newMcbData.currentR ^ newMcbData.currentL);
+            uint16_t newChecksum = CalcCRC((uint8_t*)&newMcbData,sizeof(newMcbData) - 2);
 
-        newChecksum = (uint16_t)(newMcbData.start ^ newMcbData.cmd1 ^ newMcbData.cmd2 ^ newMcbData.speedR_meas ^ newMcbData.speedL_meas 
-                                       ^ newMcbData.batVoltage ^ newMcbData.boardTemp ^ newMcbData.cmdLed ^ newMcbData.isCharging ^ newMcbData.currentR ^ newMcbData.currentL);
-        if( newChecksum == newMcbData.checksum){
-            // printf("Nuevo paquete OK -> voltage: %d\tposL: %ld\tposR:%ld\n",newMcbData.batVoltage,newMcbData.posL,newMcbData.posR);
-            xQueueSend(newMcbQueueHandler,&newMcbData,0);
-        }
-        // else {
-        //     ESP_LOGI(TAG,"Error checksum");
-        // }
+            if (newChecksum == newMcbData.checksum) {
+                // printf("Nuevo paquete OK -> voltage: %d\tposL: %ld\tposR:%ld\n",newMcbData.batVoltage,newMcbData.posL,newMcbData.posR);
+                xQueueSend(newMcbQueueHandler,&newMcbData,0);
+            }
+            else {
+                ESP_LOGI(TAG,"Error checksum-> receive: %x, calc: %x",newMcbData.checksum, newChecksum);
+            }
+        #else
+        //TODO: no falta posL y posR?
+
+
+        // Feedback.checksum   = (uint16_t)(Feedback.start ^ Feedback.cmd1 ^ Feedback.cmd2 ^ Feedback.speedR_meas ^ Feedback.speedL_meas 
+        //     ^ Feedback.batVoltage ^ Feedback.boardTemp ^ Feedback.cmdLed ^ Feedback.isCharging ^ Feedback.currentR ^ Feedback.currentL);
+
+
+        uint16_t newChecksum = (uint16_t)(newMcbData.start ^ newMcbData.cmd1 ^ newMcbData.cmd2 ^ newMcbData.speedR_meas ^ newMcbData.speedL_meas 
+                                         ^ newMcbData.batVoltage ^ newMcbData.boardTemp ^ newMcbData.statusCode ^ newMcbData.isCharging ^ newMcbData.currentR ^ newMcbData.currentL);
+            if( newChecksum == newMcbData.checksum){
+                // printf("Nuevo paquete OK -> voltage: %d\tposL: %ld\tposR:%ld\n",newMcbData.batVoltage,newMcbData.posL,newMcbData.posR);
+                xQueueSend(newMcbQueueHandler,&newMcbData,0);
+            }
+            else {
+                ESP_LOGI(TAG,"Error checksum-> receive: %x, calc: %x",newMcbData.checksum, newChecksum);
+            }
+        #endif
     }
 }
 
@@ -103,12 +153,14 @@ static void controlHandler(void *pvParameters) {
                         uart_flush_input(mcbConfigInit.numUart);
                         // Reiniciar la cola de patrones si está llena
                         uart_pattern_queue_reset(mcbConfigInit.numUart, PATTERN_QUEUE_SIZE);
+
                     } else {
                         uint8_t discard[50];         // TODO: mejorar metodo de descarte, estoy usando memoria inutilmente
                         uart_read_bytes(mcbConfigInit.numUart, discard, pos,0);   // just to discard previous bytes
                         uart_read_bytes(mcbConfigInit.numUart, data, sizeof(rx_motor_control_board_t),0);
                         processMCBData(data);
                         uart_flush_input(mcbConfigInit.numUart);
+                        uart_pattern_queue_reset(mcbConfigInit.numUart, PATTERN_QUEUE_SIZE);
                     }
                 break;
                 default:
--- include/CAN_MCB.h
+++ include/CAN_MCB.h
@@ -4,11 +4,16 @@
 #include <stdio.h>
 #include "driver/gpio.h"
 #include "driver/uart.h"
+#include "../../../include/main.h"
 
-#define MAINBOARD_BAUDRATE          921600      // 230400
+#ifdef HARDWARE_SPLITBOARD
+    #define MAINBOARD_BAUDRATE          19200
+#else
+    #define MAINBOARD_BAUDRATE          230400 // 921600
+#endif 
+
 #define START_CODE_HEADER           0xABCD
 #define START_CODE_HEADER_PATTERN   0xCD        // just one byte pattern, litle endian
-#define ID_MOTOR_MODULE     0xF0
 
 // MotorControlBoard = MCB
 
@@ -20,33 +25,74 @@ typedef struct {
     uint8_t core;
 }config_init_mcb_t;
 
-typedef struct {
-    uint16_t start;
-    int16_t  speedR;
-    int16_t  speedL;
-    uint8_t enable;
-    uint16_t checksum;
-} tx_motor_control_board_t;
+typedef enum {
+    NO_ERROR_MCB,
+    ERROR_MCB_BATTERY,
+    ERROR_MCB_TEMP,
+    ERROR_MCB_HALL_L,
+    ERROR_MCB_HALL_R,
+    ERROR_MCB_INACTIVITY,
+} status_code_mcb_t;
 
-typedef struct {
-    uint16_t  start;
-    int16_t   cmd1;
-    int16_t   cmd2;
-    int16_t   speedR_meas;
-    int16_t   speedL_meas;
-    int32_t   posR;
-    int32_t   posL;
-    int16_t   currentR;
-    int16_t   currentL;
-    int16_t   batVoltage;
-    int16_t   boardTemp;
-    uint16_t  cmdLed;
-    uint8_t   isCharging;
-    uint16_t  checksum;
+#ifdef HARDWARE_SPLITBOARD
 
-    // uint8_t     ordenCode;
-    // uint8_t     errorCode;
-} rx_motor_control_board_t;
+    typedef struct __attribute__((packed)) {
+        uint8_t start;
+        int16_t  speedR;
+        int16_t  speedL;
+        uint8_t  unusedStateMaster;
+        uint8_t  unusedStateSlave;
+        uint16_t checksum;
+    } tx_motor_control_board_t;
+
+    typedef struct __attribute__((packed)) {
+        uint16_t  start;
+        int16_t   speedL_meas;  // 100* km/h
+        int16_t   speedR_meas;
+        uint16_t  batVoltage;   // 100* V
+        int16_t   currentL;     // 100* A
+        int16_t   currentR;
+        int32_t   posL;
+        int32_t   posR;
+        // int16_t   boardTemp;
+        // uint16_t  cmdLed;
+        // uint8_t   isCharging;
+        uint16_t  checksum;
+
+        // uint8_t     ordenCode;
+        // uint8_t     errorCode;
+    } rx_motor_control_board_t;
+#elif defined(HARDWARE_MAINBOARD)
+    typedef struct __attribute__((packed)){
+        uint16_t start;
+        int16_t  speedR;
+        int16_t  speedL;
+        uint8_t  enable;
+        uint16_t checksum;
+    } tx_motor_control_board_t;
+
+    typedef struct __attribute__((packed)){
+        uint16_t    start;
+        int16_t     cmd1;
+        int16_t     cmd2;
+        int16_t     speedR_meas;
+        int16_t     speedL_meas;
+        int32_t     posR;
+        int32_t     posL;
+        int16_t     currentR;
+        int16_t     currentL;
+        int16_t     batVoltage;
+        int16_t     boardTemp;
+        uint8_t     statusCode;     // es status_code_mcb_t, pero por defecto ocupa mas de 1 byte un enum en GCC
+        uint8_t     isCharging;
+        uint16_t    checksum;
+        // uint8_t     ordenCode;
+    } rx_motor_control_board_t;
+#else 
+    #error Error hardware mainboard selected
+#endif 
+
+
 
 void mcbInit(config_init_mcb_t *config);
 
